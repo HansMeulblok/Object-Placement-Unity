89b20a65a6de3870f03cd91ac10722a901ff90ff
HansMeulblok <hansmeulblok4@gmail.com>
2021-11-03 13:16:56 +0000

    Added collsion detection for preview object

--- Object Placement/Assets/scripts/ObjectPlacement.cs
+++ Object Placement/Assets/scripts/ObjectPlacement.cs
@@ -4,30 +4,53 @@ using UnityEngine;
 
 public class ObjectPlacement : MonoBehaviour
 {
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private List<string> colliderTags = new List<string>();
     private Vector3 mousePos;
     private Camera cam;
-    private bool isColliding =  false;
 
-    [SerializeField] private GameObject prefab;
+    [HideInInspector] 
+    public bool isColliding =  false;
     private GameObject previewObject;
     private Color previewColour;
-    // Start is called before the first frame update
+
     void Start()
     {
         cam = Camera.main;
+
         previewObject = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity);
+
         previewColour = previewObject.GetComponent<SpriteRenderer>().color;
         previewColour.a = 0.5f;
         previewObject.GetComponent<SpriteRenderer>().color = previewColour;
 
+        PreviewObject pv = previewObject.AddComponent<PreviewObject>();
+        pv.tags = colliderTags;
+        pv.objectPlacement = this.gameObject.GetComponent<ObjectPlacement>();
+
         if(previewObject.GetComponent<Collider2D>() == null)
         {
             Debug.Log("added collider");
-            previewObject.AddComponent<BoxCollider2D>();
+            BoxCollider2D col = previewObject.AddComponent<BoxCollider2D>();
+            col.isTrigger = true;
+        }
+        else
+        {
+            previewObject.GetComponent<Collider2D>().isTrigger = true;
+        }
+
+        if(previewObject.GetComponent<Rigidbody2D>() == null)
+        {
+            Rigidbody2D rb = previewObject.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            rb.drag = 0;
+        }
+        else
+        {
+            previewObject.GetComponent<Rigidbody2D>().gravityScale = 0;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
---
+++ Object Placement/Assets/scripts/PreviewObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewObject : MonoBehaviour
+{
+    [HideInInspector]   
+    public List<string> tags = new List<string>();
+    [HideInInspector]
+    public ObjectPlacement objectPlacement;
+    private string currentTag;
+    private SpriteRenderer sprite;
+    private Color originalColor;
+ 
+
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        originalColor = sprite.color;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        for (int i = 0; i < tags.Count; i++)
+        {
+            if(tags[i] == other.tag)
+            {
+                // Colour red
+                sprite.color = new Color (255, 0, 0, 0.5f);
+                objectPlacement.isColliding = true;
+                // save string
+                currentTag = other.tag;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag(currentTag))
+        {
+            objectPlacement.isColliding = false;
+            sprite.color = originalColor;
+        }
+    }
+}
